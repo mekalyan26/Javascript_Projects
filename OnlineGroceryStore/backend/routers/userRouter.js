58e7619d34ec0e9e65bb8499c4782ebc63c36ab1
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-09-24 22:32:58 -0400

    signin module

--- OnlineGroceryStore/backend/routers/userRouter.js
+++ OnlineGroceryStore/backend/routers/userRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
 import User from "../models/userModels.js";
 import data from "../data.js";
 
@@ -12,6 +13,23 @@ userRouter.get(
     const createdUser = await User.insertMany(data.users);
     res.send({ createdUser });
   })
+);
+
+userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({email: req.body.email});
+    if(user){
+        if(bcrypt.compareSync(req.body.password, user.password){
+            res.send({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user)
+            });
+        }
+    }
+    res.status(401).send({message:'Invalid email or password'});
+})
 );
 
 export default userRouter;
