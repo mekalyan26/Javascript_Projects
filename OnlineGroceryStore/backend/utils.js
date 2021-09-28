41a41c0d12a710ea00ef54cf9196672352dfa4fe
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-09-27 21:59:12 -0400

    jwt implementation

--- OnlineGroceryStore/backend/utils.js
+++ OnlineGroceryStore/backend/utils.js
@@ -1,3 +1,14 @@
+import jwt from jsonwebtoken
+
 export const generateToken = (user) => {
-    return jwt.sign({_id: user._id, name: user.customer_name , email: user.email, isAdmin: user.isAdmin}, process.env.JWT_SECRET, {expiresIn:'30d',})
-}
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.customer_name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "30d" }
+  );
+};
