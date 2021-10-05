c63219b46bbcd7c16141ce8664679f043a36c350
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-10-05 08:12:55 -0400

    adding register screen functionalities

--- OnlineGroceryStore/backend/routers/userRouter.js
+++ OnlineGroceryStore/backend/routers/userRouter.js
@@ -4,35 +4,57 @@ import bcrypt from "bcryptjs";
 import Customer from "../models/userModels.js";
 import data from "../data.js";
 import { generateToken } from "../utils.js";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userRouter = express.Router();
 
 userRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
-      //await User.remove({});
+    //await User.remove({});
     const createdUser = await Customer.insertMany(data.Customer);
     res.send({ createdUser });
   })
 );
 
-userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
-    const user = await Customer.findOne({email: req.body.email});
-    if(user){
-        if(bcrypt.compareSync(req.body.password, user.password)){
-            res.send({
-                _id: user._id,
-                name: user.customer_name,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                token: generateToken(user)
-            });
-            return;
-        }
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await Customer.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.customer_name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
     }
-    res.status(401).send({message:'Invalid email or password'});
-})
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
+
+userRouter.post(
+  "/register",
+  expressAsyncHandler(async (req, res) => {
+    const user = new Customer({
+      customer_name: req.body.customer_name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+
+    const createdUser = await Customer.save();
+    res.send({
+      _id: user._id,
+      name: createdUser.customer_name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser),
+    });
+  })
 );
 
 export default userRouter;
---
+++ OnlineGroceryStore/frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect }  from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../actions/userActions.js";
+
+export default function RegisterScreen(props) {
+  const [customer_name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const redirect = props.location.search
+    ? props.locations.search.split("=")[1]
+    : "/";
+
+    const userRegister = useSelector((state) => state.userRegister);
+    const { userInfo, loading, error} = userRegister;
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(register(customer_name, email, password));
+  };
+  useEffect( () => {
+    if(userInfo){
+      props.history.push(redirect);
+    }
+
+  }, [userInfo]);
+  return (
+    <div>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Register</h1>
+        </div>
+        <div>
+          <label htmlFor="customer_name">Customer Name</label>
+          <input
+            type="text"
+            id="customer_name"
+            placeholder="Enter name"
+            required
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label />
+          <button className="button primary" type="submit">
+            Sign In
+          </button>
+        </div>
+        <div>
+          <label />
+          <div>
+            Already have an account? <Link to="/signin">Sign-In</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
