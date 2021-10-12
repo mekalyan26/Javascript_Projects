a4fbb84fbfad71a0879786d1676bbd68743dbec3
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-10-12 08:37:12 -0400

    payment method screen functionality

---
+++ OnlineGroceryStore/frontend/src/screens/paymentMethodScreen.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import CheckoutSteps from "../components/CheckoutSteps";
+
+export default function PaymentMethodScreen(props) {
+
+    const [paymentMethod, setPaymentMethod] = useState('PayPal');
+    const dispatch = useDispatch();
+    const submitHandler = (e) =>{
+        e.preventDefault();
+        dispatch(savePaymentMethod(paymentMethod));
+        props.history.push('/placeholder')
+
+    }
+
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Payment</h1>
+        </div>
+        <div>
+          <div>
+            <input
+              type="radio"
+              name="paymentMethod"
+              value="paypal"
+              id="paypal"
+              required
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></input>
+            <label htmlfor="paypal">PayPal</label>
+          </div>
+        </div>
+        <div>
+          <div>
+            <input
+              type="radio"
+              name="paymentMethod"
+              value="Stripe"
+              id="stripe"
+              required
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></input>
+            <label htmlfor="stripe">PayPal</label>
+          </div>
+        </div>
+        <div>
+            <button className="primary" type="submit">Continue</button>
+        </div>
+      </form>
+    </div>
+  );
+}
