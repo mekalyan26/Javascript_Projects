0dafe9ea33fa181c96bfaf56e282da3e76736a0d
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-11-26 06:48:58 -0500

    use effect functionality

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,25 +1,36 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
+import { uuid } from "uuidv4";
 
 function App() {
+  // {id:"1", name:"Plumber", email:"plumber@gmail.com" },
+  // {id:"2", name:"Builder", email:"builder@gmail.com" }
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
 
-    // {id:"1", name:"Plumber", email:"plumber@gmail.com" },
-    // {id:"2", name:"Builder", email:"builder@gmail.com" }
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
-    const [contacts, setContacts ] = useState([]);
-    const addContactHandler =(contact) => {
-      console.log(contact);
-    }
-  
-  
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
   return (
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts}/>
+      <ContactList contacts={contacts} />
     </div>
   );
 }
